7d91ceda2aaf304c12441f27132a6ec43775fee1
iamdpegg <pegg.david.a@gmail.com>
2023-03-20 01:01:45 -0400

    Add prototype files

---
+++ HasBeen/Destination/DestinationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostViewController.swift
+//  BeReal
+//
+//  Created by David Pegg on 2/24/23.
+//
+
+import UIKit
+
+class DestinationViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
---
+++ HasBeen/Login/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  BeReal
+//
+//  Created by David Pegg on 2/24/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+

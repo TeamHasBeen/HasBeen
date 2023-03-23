ea9bf4cf65956ba32410645aca13a9a848fcf5d0
iamdpegg <pegg.david.a@gmail.com>
2023-03-22 20:19:37 -0400

    Add project framework

---
+++ HasBeen/Creation/CreationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FeedViewController.swift
+//  BeReal
+//
+//  Created by David Pegg on 2/24/23.
+//
+
+import UIKit
+
+
+
+class CreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+}
+
---
+++ HasBeen/Login/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  BeReal
+//
+//  Created by David Pegg on 2/24/23.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

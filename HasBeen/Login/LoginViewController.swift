be6a51b8d15efb48dab9ec9568b29b0f068e4315
efrars <erodr602@fiu.edu>
2023-03-26 16:49:29 -0400

    Add log-in and sign-up functionality

--- HasBeen/Login/LoginViewController.swift
+++ HasBeen/Login/LoginViewController.swift
@@ -17,5 +17,41 @@ class LoginViewController: UIViewController {
 
     }
     
+    @IBAction func onLoginTapped(_ sender: Any) {
+        guard let username = usernameField.text,
+              let password =  passwordField.text,
+              !username.isEmpty,
+              !password.isEmpty else {
+            showMissingFieldAlert()
+            return
+        }
+        
+        User.login(username: username, password: password) {[weak self] result in
+            switch result {
+            case .success(let user):
+                print("✅ Successfully logged in as user: \(user)")
+                
+                NotificationCenter.default.post(name: Notification.Name("Login"), object: nil)
+            case .failure(let error):
+                self?.showAlert(description: error.localizedDescription)
+            }
+        }
+    }
+    
+    private func showAlert(description: String?) {
+        let alertController = UIAlertController(title: "Unable to Log in", message: description ?? "Unknown error", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+    
+    
+    private func showMissingFieldAlert() {
+        let alertController = UIAlertController(title: "Error.", message: "All fields need to be completed in order to log in.", preferredStyle: .alert)
+        let action = UIAlertAction(title:"OK", style: .default)
+        alertController.addAction(action)
+        present(alertController,animated: true)
+    }
+    
 }
 

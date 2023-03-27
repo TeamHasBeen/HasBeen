95919151c4dd585f5ce1c50de867eec21af5d8b1
iamdpegg <pegg.david.a@gmail.com>
2023-03-26 22:19:30 -0400

    Add signout functionality

--- HasBeen/Creation/CreationViewController.swift
+++ HasBeen/Creation/CreationViewController.swift
@@ -9,6 +9,21 @@ class CreationViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
+    
+    @IBAction func onLogoutTapped(_ sender: Any) {
+        showConfirmLogoutAlert()
+    }
+    
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Log out of \(User.current?.username ?? "current account")?", message: nil, preferredStyle: .alert)
+        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(logOutAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
 
 }
 

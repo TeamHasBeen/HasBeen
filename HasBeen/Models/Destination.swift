e91081eacf0a06348b3024b8fcfe2cfad847c139
iamdpegg <pegg.david.a@gmail.com>
2023-03-26 22:03:40 -0400

    Adjust model fields for sprint 2

--- HasBeen/Models/Destination.swift
+++ HasBeen/Models/Destination.swift
@@ -1,33 +1,17 @@
 import ParseSwift
 import Foundation
 
-// Fields are tentative
-// https://developers.skyscanner.net/docs/geo/overview
+// Won't be involved with any API calls, only needs to conform to
+// Parse protocol.
 
-struct GeoInfo: ParseObject {
+struct Destination: ParseObject {
     var objectId: String?
     var createdAt: Date?
     var updatedAt: Date?
     var ACL: ParseACL?
     var originalData: Data?
 
-    var entityId: String?
-    var parentId: String?
-    var name: String?
-    var type: String?
-    var iata: String?
-    var latitude: Int?
-    var longitude: Int?
-}
-
-struct Market: ParseObject {
-    var objectId: String?
-    var createdAt: Date?
-    var updatedAt: Date?
-    var ACL: ParseACL?
-    var originalData: Data?
-
-    var code: String?
-    var name: String?
-    var currency: String?
+    var city: String?
+    var country: String?
+    var continent: String?
 }

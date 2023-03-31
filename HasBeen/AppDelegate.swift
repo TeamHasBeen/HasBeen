323bc550cd6bb0aed756155a4da340cf6a48c7de
efrars <erodr602@fiu.edu>
2023-03-31 16:14:06 -0400

    Stored-Destinations
    This code will save the Destinations that were declared in the Destionation swift file. If any property of destination is repeated, then it won't be stored.

--- HasBeen/AppDelegate.swift
+++ HasBeen/AppDelegate.swift
@@ -11,6 +11,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         ParseSwift.initialize(applicationId: "bTrjfEICGRLTVyZfXaUkUNnunMI1hUkfnltobfLD",
                                  clientKey: "1UXSRabB76C5pqk8XhyCR11PFG5nKGlBS9YBlq4Q",
                                  serverURL: URL(string: "https://parseapi.back4app.com")!)
+        
+        // Create an array to store the new Destination objects that need to be saved
+        var newDestionationsArray: [Destination] = []
+        
+        // Create a DispatchGroup to synchronize the asynchronous tasks
+        let group = DispatchGroup()
+        
+        // Loop through the destionationsArray and check if each object is already in the database
+        
+        for destination in Destination.destinationsArray {
+            // Enter the DispatchGroup
+            
+            group.enter()
+            
+            // Create a query to check if the destionation is already in the database
+            let query = Destination.query("city" == destination.city)
+            query.find { results in
+                switch results {
+                case .success(let destinations):
+                    if destinations.isEmpty {
+                        // The query did not return any results
+                        newDestionationsArray.append(destination)
+                    }
+                case .failure(let error):
+                    print("Error checking if destination is in database: \(error.localizedDescription)")
+
+                }
+                
+                // Leave the DispatchGroup
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: .main) {
+            // Save the new Destination objects that were not found in the database
+            if !newDestionationsArray.isEmpty {
+                newDestionationsArray.saveAll() { result in
+                    switch result {
+                    case .success( _ ):
+                        print("Successfuly saved destinations")
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
+        
         return true
     }
 

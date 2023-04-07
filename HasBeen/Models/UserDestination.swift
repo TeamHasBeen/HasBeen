b247d40eb6505d19a2737977de4f37f1dee9e7d7
SYRE33 <117843723+SYRE33@users.noreply.github.com>
2023-04-07 19:35:23 -0400

    Add Place and UserDestination models

--- HasBeen/Models/UserDestination.swift
+++ HasBeen/Models/UserDestination.swift
@@ -1,1 +1,14 @@
 import Foundation
+import ParseSwift
+
+struct UserDestination: ParseObject {
+    
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    var user: Pointer<User>?
+    var destination: Pointer<Destination>?
+}
